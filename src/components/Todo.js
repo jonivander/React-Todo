7ca125b083806ebfd534503532458527388327bc
jonathan warner <j.m.warners@gmail.com>
2020-08-03 15:45:40 -0700

    MVP complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Todo.css'
 
 const Item = props => {
     return (
         <div
             onClick={() => props.toggleItem(props.item.id)}
-            className={`item${props.item.completed ? "completed" : ""}`}
+            className={`item${props.item.completed ? " completed" : ""}`}
         >
             <p>{props.item.name}</p>
         </div>
